5bad10306a1eec6bab2d47bf50d9ed09f1fec06c
KOLA <koladr8621@gmail.com>
2024-03-22 14:45:27 +0700

    com

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return ('welcome to Online Task Management System with Laravel API Server');
 });
+
+Route::get('/home', function () {
+    return ('This Home');
+});
