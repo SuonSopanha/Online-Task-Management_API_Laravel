1f3cbcf041d7f7d7147ee591209ec98e22e1cb01
Sopanha <129533258+SuonSopanha@users.noreply.github.com>
2024-04-13 13:28:23 +0700

    add UserCRUD

--- app/Traits/HttpResponses.php
+++ app/Traits/HttpResponses.php
@@ -13,7 +13,7 @@ trait HttpResponses {
         ],$code);
     }
 
-    protected function error ($data, $message = null,$code){
+    protected function error($data, $message = null,$code){
         return response()->json([
             'status' => "Error has occurred",
             'message' => $message,
