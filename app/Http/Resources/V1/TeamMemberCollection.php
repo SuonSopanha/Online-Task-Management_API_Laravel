e18137e8965c92a67f1db06bf6a568846d0aa5ba
RatanaJr <mamsovanratana09@gmail.com>
2024-04-25 11:15:50 +0700

    Update TeamMemberCollection.php

--- app/Http/Resources/V1/TeamMemberCollection.php
+++ app/Http/Resources/V1/TeamMemberCollection.php
@@ -10,10 +10,21 @@ class TeamMemberCollection extends ResourceCollection
     /**
      * Transform the resource collection into an array.
      *
+     * @param  \Illuminate\Http\Request  $request
      * @return array<int|string, mixed>
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection->map(function ($teamMember) {
+                return [
+                    'id' => $teamMember->id,
+                    'email' => $teamMember->email,
+                    'full_name' => $teamMember->full_name,
+                    'photo_url' => $teamMember->photo_url,
+                ];
+            }),
+        ];
     }
 }
+
