579826b5516c06c8cffbd3855dc59100fbb8d18b
SkyK.O <149860914+KOLADDR9@users.noreply.github.com>
2024-04-25 16:25:31 +0700

    update

--- app/Http/Resources/V1/GoalResource.php
+++ app/Http/Resources/V1/GoalResource.php
@@ -15,12 +15,15 @@ class GoalResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            
-                'id' => $this->id,
-                'team_id' => $this->team_id,
-                'goal_name' => $this->goal_name,
-                'description' => $this->description,
-                'completed' => $this->completed
+            $this->collection->map(function ($goal){
+                return [
+                    'id' => $goal->id,
+                    'team_id' => $goal->team_id,
+                    'goal_name' => $goal->goal_name,
+                    'description' => $goal->description,
+                    'completed' => $goal->completed
+                ];
+            }),
         ];
         
     }
