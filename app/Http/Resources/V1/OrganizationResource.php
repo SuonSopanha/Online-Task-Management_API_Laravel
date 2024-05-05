75e6a70ad7e427041307bf3cbcc15885f65ff08b
Sopanha <129533258+SuonSopanha@users.noreply.github.com>
2024-05-05 23:38:51 +0700

    add organization resource and collection

--- app/Http/Resources/V1/OrganizationResource.php
+++ app/Http/Resources/V1/OrganizationResource.php
@@ -14,6 +14,13 @@ class OrganizationResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'industry' => $this->industry,
+            'owner_id' => $this->owner_id,
+            'email' => $this->email,
+        ];
     }
 }
