24dacf29055ef0f3fc344aea05914cf2fb5260ea
Sopanha <129533258+SuonSopanha@users.noreply.github.com>
2024-05-12 22:35:14 +0700

    update 12 May

--- app/Http/Controllers/V1/OrganizationController.php
+++ app/Http/Controllers/V1/OrganizationController.php
@@ -2,10 +2,51 @@
 
 namespace App\Http\Controllers\V1;
 
-use App\Http\Controllers\Controller;
+use App\Models\Organization;
 use Illuminate\Http\Request;
+use App\Traits\HttpResponses;
+use App\Http\Controllers\Controller;
+use App\Services\V1\OrganizationQuery;
 
 class OrganizationController extends Controller
 {
-    //
+    use HttpResponses;
+
+
+    public function index(Request $request)
+    {
+        $filter = new OrganizationQuery();
+        $query = $filter->transform($request);
+        return $this->success($query);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'description' => 'nullable|string',
+        ]);
+        $organization = Organization::create($data);
+        return $this->success($organization, 'Organization created successfully');
+    }
+
+
+    public function update(Request $request, Organization $organization)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'description' => 'nullable|string',
+        ]);
+        $organization->update($data);
+        return $this->success($organization, 'Organization updated successfully');
+    }
+
+
+    public function destroy(Organization $organization)
+    {
+        $organization->delete();
+        return $this->success([], 'Organization deleted successfully');
+    }
+
+
 }
---
+++ app/Services/V1/OrgMemberQuery.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services\V1;
+
+use App\Models\MileStone;
+use Illuminate\Http\Request;
+
+class OrgMemberQuery
+{
+
+}
