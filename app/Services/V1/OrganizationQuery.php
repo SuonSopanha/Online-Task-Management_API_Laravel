27c044a3a9d474086c8627d79698f143c2786884
Sopanha <129533258+SuonSopanha@users.noreply.github.com>
2024-05-13 22:44:37 +0700

    add organization query

--- app/Services/V1/OrganizationQuery.php
+++ app/Services/V1/OrganizationQuery.php
@@ -2,13 +2,48 @@
 
 namespace App\Services\V1;
 
-use App\Models\MileStone;
+use App\Models\Organization;
 use Illuminate\Http\Request;
 
-
 class OrganizationQuery {
+
+    protected $safeParams = [
+        'id' => ['eq'],
+        'name' => ['eq', 'like'],
+        'industry' => ['eq', 'like'],
+        'owner_id' => ['eq'],
+    ];
+
+    protected $columnMap = [
+        'id' => 'id',
+        'name' => 'name',
+        'industry' => 'industry',
+        'owner_id' => 'owner_id',
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'like' => 'like',
+    ];
+
     public function transform(Request $request)
     {
+        $eloQuery = Organization::query();
 
+        foreach ($this->safeParams as $param => $operators) {
+            $query = $request->query($param);
+            if (!isset($query)) {
+                continue;
+            }
+            $column = $this->columnMap[$param] ?? $param;
+            foreach ($operators as $operator) {
+                if (isset($query[$operator])) {
+                    $value = $operator === 'like' ? '%' . $query[$operator] . '%' : $query[$operator];
+                    $eloQuery->where($column, $this->operatorMap[$operator] ?? $operator, $value);
+                }
+            }
+        }
+
+        return $eloQuery;
     }
 }
